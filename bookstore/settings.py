c24cd0d20888cfb4b3312af44d88f221129918b3
Jay Hernandez <ajuan_jose@hotmail.com>
2020-09-28 05:45:39 -0600

    About Page tests

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -39,9 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #Third Party
+    'crispy_forms',
+
+
     #Local
     'users.apps.UsersConfig',
-    'pages.apps.PagesConfig'
+    'pages.apps.PagesConfig',
+
+  
 ]
 
 MIDDLEWARE = [
@@ -128,5 +134,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_FINDERS = ['django.contrib.staticfiles.finders.FileSystemFinder', 
+                        'django.contrib.staticfiles.finders.AppDirectoriesFinder']
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
