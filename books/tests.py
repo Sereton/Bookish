89b4c2bb45e9c25eaa1c62e15d8593a52b7f2de0
Jay Hernandez <ajuan_jose@hotmail.com>
2020-09-29 04:43:52 -0600

    Reviews added to book model

--- books/tests.py
+++ books/tests.py
@@ -1,15 +1,28 @@
+from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
-from .models import Book
+from .models import Book, Review
 
 class BookTests(TestCase):
 
     def setUp(self):
+        self.user = get_user_model().objects.create(
+            username = 'reviewuser',
+            email= 'reviewer@mail.com',
+            password ='pass1234'
+        )
+
         self.book = Book.objects.create(
                     title='Harry Potter',
                     author='JK Rowling',
                     price='25.00',
                     )
+
+        self.review = Review.objects.create(
+            book= self.book,
+            author=self.user,
+            review= "Pretyy, Pretaayy good"
+        )
 
     def test_book_listing(self):
         self.assertEqual(f'{self.book.title}', 'Harry Potter')
@@ -30,5 +43,6 @@ class BookTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'Harry Potter')
         self.assertTemplateUsed(response, 'books/book_detail.html')
+        self.assertContains(response,'Pretyy, Pretaayy good')
 
 
